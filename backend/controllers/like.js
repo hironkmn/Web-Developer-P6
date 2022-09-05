c62e9d00247bae1c67ca2d667bc9b2244bb29631
Meïdine Zerrouk <meidine59@gmail.com>
2022-09-05 15:01:46 +0200

    Final update

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -5,15 +5,13 @@ exports.addLikes = (req, res, next) => {
         .then((like) => {
             if (!like.usersLiked.includes(req.body.userId) && req.body.like === 1){
                 Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: 1 }, $push: { usersLiked: req.body.userId }})
-                console.log(req.body.userId)
                     .then(() => res.status(201).json({ message: "Sauce like +1" }))
                     .catch((error) => res.status(400).json({ error }))
             } else if (like.usersLiked.includes(req.body.userId) && req.body.like === 0){
                 Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: -1 }, $pull: { usersLiked: req.body.userId }})
                     .then(() => res.status(201).json({ message: "Sauce like 0" }))
-                    console.log('----> J ai bien désincrémenté')
                     .catch((error) => res.status(400).json({ error }))
-            } else if (!like.usersDisliked.includes(req.body.userId) && req.body.like === 1){
+            } else if (!like.usersDisliked.includes(req.body.userId) && req.body.like === -1){
                 Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: 1 }, $push: { usersDisliked: req.body.userId }})
                     .then(() => res.status(201).json({ message: "Sauce dislike +1" }))
                     .catch((error) => res.status(400).json({ error }))
