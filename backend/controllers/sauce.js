8ba6827a9cb539c3205a6a3808fea7ad48789a73
Meïdine Zerrouk <meidine59@gmail.com>
2022-09-22 15:11:13 +0200

    Last update for the fun

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -15,13 +15,13 @@ exports.createSauce = (req, res, next) => {
   
     sauce.save()
     .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
-    .catch(error => { res.status(400).json( { error })})
+    .catch(error => { res.status(500).json( { error })})
  }
 
  exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({ error }))
+        .catch(error => res.status(500).json({ error }))
 }
 
  exports.modifySauce = (req, res, next) => {
@@ -38,11 +38,11 @@ exports.createSauce = (req, res, next) => {
             } else {
                 Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
                 .then(() => res.status(200).json({message : 'Objet modifié!'}))
-                .catch(error => res.status(401).json({ error }))
+                .catch(error => res.status(500).json({ error }))
             }
         })
         .catch((error) => {
-            res.status(400).json({ error })
+            res.status(500).json({ error })
         })
  }
 
@@ -56,7 +56,7 @@ exports.createSauce = (req, res, next) => {
                 fs.unlink(`images/${filename}`, () => {
                     Sauce.deleteOne({_id: req.params.id})
                         .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
-                        .catch(error => res.status(401).json({ error }))
+                        .catch(error => res.status(500).json({ error }))
                 })
             }
         })
@@ -68,5 +68,5 @@ exports.createSauce = (req, res, next) => {
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }))
+        .catch(error => res.status(500).json({ error }))
 }
